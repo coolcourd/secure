53cafb4a00644ad47bc57e7101adae25a92ba88d
courd <courdheadman@gmail.com>
2020-08-10 21:46:19 -0700

    font

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const foot = (prev, next) => {
     const footer = document.createElement("footer")
-    footer.innerHTML = `
+    const header = document.createElement("header")
+    const theHtml = `
     <p>
     <a href="${prev}">back</a>
     <a href="/">home</a>
     <a href="${next}">next</a>
     </p>
     `
+    footer.innerHTML = theHtml
+    header.innerHTML = theHtml
+    document.body.appendChild(header)
     document.body.appendChild(footer)
 }
